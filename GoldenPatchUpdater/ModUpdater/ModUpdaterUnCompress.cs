94f6f18c751d35dd2cee2847db6dfd9d8a15a5bd
tenshoukijp <komiyama_a@hotmail.com>
2022-05-09 01:13:20 +0900

    独立したリポジトリへ

---
+++ GoldenPatchUpdater/ModUpdater/ModUpdaterUnCompress.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.GZip;
+
+
+namespace ModUpdater
+{
+    partial class ModUpdaterForm : Form
+    {
+
+        /// <summary>
+        ///  GZipファイルの解凍
+        /// </summary>
+        /// <param name="filename">相対(または絶対)ファイル名</param>
+        private void extractGZip(String gzipFileName)
+        {
+
+            String inFile = gzipFileName;
+
+                // 入力ファイルは.gzファイルのみ有効
+                if (!inFile.ToLower().EndsWith(".gz"))
+                {
+                    return;
+                }
+
+                // ファイル名末尾の「.gz」を削除
+                string outFile = inFile.Substring(0, inFile.Length - 3);
+
+                int num;
+                byte[] buf = new byte[1024]; // 1Kbytesずつ処理する
+
+                // 入力ストリーム
+                FileStream inStream = new FileStream(inFile, FileMode.Open, FileAccess.Read);
+    
+                // 解凍ストリーム
+                GZipStream decompStream  = new GZipStream(
+                    inStream, // 入力元となるストリームを指定
+                    CompressionMode.Decompress); // 解凍（圧縮解除）を指定
+
+                 // 出力ストリーム
+                FileStream outStream = new FileStream(outFile, FileMode.Create);
+
+                using (inStream)
+                using (outStream)
+                using (decompStream)
+                {
+                    while ((num = decompStream.Read(buf, 0, buf.Length)) > 0)
+                    {
+                        outStream.Write(buf, 0, num);
+                    }
+                }
+
+                if (File.Exists(outFile))
+                {
+                    extractTar(outFile);
+                } else {
+                    sbProgress.Text = "ファイル解凍中にエラーが発生しました";
+                }
+            }
+
+        /// <summary>
+        ///  GZipファイルの解凍
+        /// </summary>
+        /// <param name="filename">相対(または絶対)ファイル名</param>
+        private void extractTar(String tarFileName)
+        {
+            String fullTarPath = System.IO.Path.GetFullPath(tarFileName);
+            TarInputStream tis1 = new TarInputStream(File.OpenRead(fullTarPath));
+            TarArchive archive = TarArchive.CreateInputTarArchive(tis1);
+
+            archive.SetKeepOldFiles(false);
+            archive.AsciiTranslate = false;
+            archive.SetUserInfo(0, "", 0, "None");
+
+            String fullDirName = System.IO.Path.GetFullPath(strTmpDir);
+            archive.ExtractContents(fullDirName);
+            archive.Close();
+            tis1.Close();
+
+
+            //ZIP内のエントリを列挙
+            ICSharpCode.SharpZipLib.Tar.TarEntry te;
+
+            TarInputStream tis2 = new TarInputStream(File.OpenRead(fullTarPath));
+            //ZipEntryを取得
+            while ((te = tis2.GetNextEntry()) != null)
+            {
+                if (te.IsDirectory)
+                {
+                    //ディレクトリのとき
+                    Console.WriteLine("ディレクトリ名 : {0}", te.Name);
+                    Console.WriteLine("日時 : {0}", te.ModTime);
+                    Console.WriteLine();
+                }
+                //情報を表示する
+                else
+                {
+                    //ファイルのとき
+                    Console.WriteLine("名前 : {0}", te.Name);
+                    Console.WriteLine("サイズ : {0} bytes", te.Size);
+                    System.IO.File.SetLastWriteTime(strTmpDir + @"\" + te.Name, te.ModTime.ToLocalTime()); // タイムスタンプを修正
+                    Console.WriteLine("日時 : {0}", te.ModTime);
+                    Console.WriteLine();
+                }
+            }
+
+            tis2.Close();
+        }
+    }
+}
---
+++ GoldenPatchUpdater/ModUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("天翔記 金パッチ 更新システム")]
+[assembly: AssemblyDescription("天翔記 金パッチ 更新システム")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("天翔記 金パッチ 更新システム")]
+[assembly: AssemblyCopyright("Copyright ©  2015 天翔記.jp")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("1861937b-1a24-46c9-8f3c-9407cd0e999c")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ja-JP")]
